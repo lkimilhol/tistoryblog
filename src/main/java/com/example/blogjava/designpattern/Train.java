3d954d7a97e6dae5df1f307e7e63f544db7bcade
lkimilhol <lkimilhol@gmail.com>
2022-04-03 17:07:13 +0900

    팩토리 패턴을 위한 예제 코드

---
+++ src/main/java/com/example/blogjava/designpattern/Train.java
@@ -0,0 +1,21 @@
+package com.example.blogjava.designpattern;
+
+public class Train extends Transportation{
+    Train(String name, String type, String fuel) {
+        this.name = name;
+        this.type = type;
+        this.fuel = fuel;
+    }
+
+    @Override
+    public String explain() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append("는(은) ");
+        sb.append(type);
+        sb.append("이며 ");
+        sb.append(fuel);
+        sb.append("로(으로) 움직입니다.");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/example/blogjava/designpattern/Transportation.java
@@ -0,0 +1,9 @@
+package com.example.blogjava.designpattern;
+
+public abstract class Transportation {
+    String name;
+    String type;
+    String fuel;
+
+    public abstract String explain();
+}
